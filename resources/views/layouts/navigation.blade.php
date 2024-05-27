d2608373fed27aef3565fa825c8997d279b2c1ae
sjwatts119 <samuel.watts67@gmail.com>
2024-05-27 20:37:34 +0100

    Link to Admin Area
    When authenticated a link to the admin area is now given to the user

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -43,6 +43,10 @@
                         </x-slot>
 
                         <x-slot name="content">
+                            <x-dropdown-link :href="route('filament.admin.pages.dashboard')">
+                                {{ __('Admin Area') }}
+                            </x-dropdown-link>
+                            
                             <x-dropdown-link :href="route('profile.edit')">
                                 {{ __('Profile') }}
                             </x-dropdown-link>
@@ -99,7 +103,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('filament.admin.pages.dashboard')" :active="request()->routeIs('filament.admin.pages.dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
