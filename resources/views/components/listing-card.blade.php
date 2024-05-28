30aa1e261c258fb35f9ab19d40d508687258406d
sjwatts119 <samuel.watts67@gmail.com>
2024-05-28 06:56:52 +0100

    Tidying up Scaling and Other Bits
    Still some weird padding but not padding going on with the text, next on the list is to move the tags to icons

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -34,19 +34,20 @@
 
 
 
-<a href="#" class="h-full flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+<a href="#" class="h-full flex flex-col items-center bg-white border border-gray-200  shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
     {{--show the listing's first image as to where it is always the same size, and maintains the same aspect ratio--}}
     <div class="overflow-hidden md:w-1/2 w-full md:h-full ">
         @if($listing->GetFirstMediaUrl())
-            <img class="object-cover w-full md:h-full m-auto" src="{{ $listing->GetFirstMediaUrl() }}" alt="{{ $listing->name }}">
+            <img class="object-cover w-full md:h-full h-96  m-auto" src="{{ $listing->GetFirstMediaUrl() }}" alt="{{ $listing->name }}">
         @else
             <img class="object-cover w-full md:h-full m-auto" src="https://via.placeholder.com/150" alt="{{ $listing->name }}">
         @endif
     </div>
-    <div class="md:w-1/2 flex flex-col justify-between p-4 leading-normal">
+    <div class="md:w-1/2 justify-between md:min-h-0 flex flex-col p-4 leading-normal">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$listing->name}}</h5>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$listing->friendlyDescription()}}</p>
-        <div class="flex flex-wrap -m-1">
+        <div class="flex flex-wrap">
+            {{--loop through attributes and show icons if they are true--}}
             @foreach($listing->attributes as $key => $value)
                 @if($value)
                     <span class="inline-block bg-green-200 rounded-full px-3 py-1 my-1 text-sm font-semibold text-gray-700 mr-1">{{$key}}</span>
