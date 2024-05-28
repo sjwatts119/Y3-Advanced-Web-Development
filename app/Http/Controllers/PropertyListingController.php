8ec3f12f8e71fe36735563d3154ba453e9446c14
sjwatts119 <samuel.watts67@gmail.com>
2024-05-28 16:03:37 +0100

    Add lightbox package

--- app/Http/Controllers/PropertyListingController.php
+++ app/Http/Controllers/PropertyListingController.php
@@ -7,12 +7,10 @@ use Illuminate\Http\Request;
 
 class PropertyListingController extends Controller
 {
-    //make a function to show all property listings
     public function index(){
         return view('properties.index', ['listings' => PropertyListing::all()]);
     }
 
-    //make a function to show a specific property listing
     public function show($slug){
         return view('properties.show', ['listing' => PropertyListing::where('slug', $slug)->first()]);
     }
--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -1,3 +1,13 @@
+{{--we have the following resources available:
+        'user_id',
+        'name',
+        'slug',
+        'description',
+        'sleeps',
+        'location',
+        'attributes',
+--}}
+
 <x-app-layout>
 
     <x-slot name="header">
@@ -5,6 +15,37 @@
             {{ "Viewing Property: " . $listing->name }}
         </h2>
     </x-slot>
+
+    {{--make a page wide container 80% width--}}
+    <div class="container mx-auto px-4 bg-white">
+
+        {{--We should display all of the images for the listing in a carousel, and we should be able to click the carousel to view all of the images in a lightbox--}}
+        <div class="flex flex-wrap">
+            <div class="w-full md:w-1/2">
+                <div class="owl-carousel owl-theme">
+                    @foreach($listing->getMedia('images') as $image)
+                        <div class="item">
+                            <img class="w-full" src="{{ $image->getUrl() }}" alt="{{ $listing->name }}">
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="w-full md:w-1/2">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$listing->name}}</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$listing->description}}</p>
+                <div class="flex flex-wrap">
+                    {{--loop through attributes and show icons if they are true--}}
+                    @foreach($listing->attributes as $key => $value)
+                        @if($value)
+                            <span class="inline-block bg-green-200 rounded-full px-3 py-1 my-1 text-sm font-semibold text-gray-700 mr-1">{{$key}}</span>
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+    </div>
+
+
+
 
 
 </x-app-layout>
