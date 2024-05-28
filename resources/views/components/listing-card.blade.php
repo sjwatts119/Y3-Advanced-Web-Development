309f7fa6d07821160f6bd63c1f380bbb0306cc13
sjwatts119 <samuel.watts67@gmail.com>
2024-05-28 01:57:13 +0100

    Card Improvement
    Images are still buggy depending on original upload size, will look at optimising this next

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -8,25 +8,28 @@
         'attributes',
 --}}
 
-<div class="max-w-sm rounded-2xl overflow-hidden shadow-lg">
-    @if($listing->GetFirstMediaUrl())
-        <img class="w-full" src="{{ $listing->GetFirstMediaUrl() }}" alt="{{ $listing->name }}">
-    @else
-        <img class="w-full" src="https://via.placeholder.com/150" alt="{{ $listing->name }}">
-    @endif
-    <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">{{ $listing->name }}</div>
-        <p class="text-gray-700 text-base">
-            {{ $listing->friendlyDescription() }}
-        </p>
-    </div>
-    <div class="px-6 py-4">
-        {{-- loop through the attributes which have been stored as json and casted to an array, with each record being in format: "key": value --}}
-        @foreach($listing->attributes as $key => $value)
-            @if($value)
-                <span class="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{{ $key }}</span>
-            @endif
-        @endforeach
+<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        @if($listing->GetFirstMediaUrl())
+            <img class="rounded-t-lg" src="{{ $listing->GetFirstMediaUrl() }}" alt="{{ $listing->name }}">
+        @else
+            <img class="rounded-t-lg" src="https://via.placeholder.com/150" alt="{{ $listing->name }}">
+        @endif
+    </a>
+    <div class="p-5">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $listing->name }}</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $listing->friendlyDescription() }}</p>
+        {{--make container for tags--}}
+        <div class="flex flex-wrap -m-1">
+            {{-- loop through the attributes which have been stored as json and casted to an array, with each record being in format: "key": value --}}
+            @foreach($listing->attributes as $key => $value)
+                @if($value)
+                    <span class="inline-block bg-green-200 rounded-full px-3 py-1 my-1 text-sm font-semibold text-gray-700 mr-1">{{ $key }}</span>
+                @endif
+            @endforeach
+        </div>
     </div>
 </div>
 
--- resources/views/properties.blade.php
+++ resources/views/properties.blade.php
@@ -2,9 +2,19 @@
 
     {{--dd($listings)--}}
 
-    @foreach($listings as $listing)
-        @include('components.listing-card', ['listing' => $listing])
-    @endforeach
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Property Listings') }}
+        </h2>
+    </x-slot>
+
+
+
+    <div class="flex flex-wrap">
+        @foreach($listings as $listing)
+            @include('components.listing-card', ['listing' => $listing])
+        @endforeach
+    </div>
 
 </x-app-layout>
 
