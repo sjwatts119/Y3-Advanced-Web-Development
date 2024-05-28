7056212767384d9bfbb00c6e5245447e29cbe5b2
sjwatts119 <samuel.watts67@gmail.com>
2024-05-28 01:45:05 +0100

    Install Flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,10 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './node_modules/flowbite/**/*.js',
     ],
+
+    //darkMode: 'selector',
 
     theme: {
         extend: {
@@ -17,5 +20,8 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        require('flowbite/plugin')
+    ],
 };
