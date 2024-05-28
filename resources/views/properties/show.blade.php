b50721521183e08c9cf35fcec257f5520513f441
sjwatts119 <samuel.watts67@gmail.com>
2024-05-28 18:05:01 +0100

    Carousel being used
    Far more user friendly and retains consistent design language across from the properties page.

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -9,35 +9,45 @@
         <div class="bg-white rounded-xl lg:w-2/3 w-full mx-auto">
 
             <div class="container md:min-h-0 flex flex-col p-4 leading-normal">
-                <div class="grid gap-4">
-                    <div>
-                        @if($listing->GetFirstMediaUrl())
-                            <a href="{{ $listing->getFirstMediaUrl() }}" class="glightbox">
-                                <img class="h-auto w-full max-h-96 overflow-hidden object-cover rounded-lg" src="{{ $listing->getFirstMediaUrl() }}" alt="{{ $listing->name }}">
-                            </a>
-                        @else
-                            <img class="h-auto max-w-full rounded-lg" src="https://via.placeholder.com/150" alt="{{ $listing->name }}">
-                        @endif
+                <div id="default-carousel" class="relative w-full" data-carousel="slide">
+                    <!-- Carousel wrapper -->
+                    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                        @foreach($listing->getMedia('default') as $media)
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <img src="{{$media->getUrl()}}" class="object-cover absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                            </div>
+                        @endforeach
+
                     </div>
-                    @php
-                        $media = $listing->getMedia();
-                    @endphp
-                    @if($media->count() > 1)
-                        <div class="md:grid grid-cols-5 gap-4 h-full hidden">
-                            @foreach($media->take(5) as $mediaItem)
-                                <a href="{{ $mediaItem->getUrl() }}" class="glightbox">
-                                    <img class="h-24 w-full overflow-hidden object-cover rounded-lg" src="{{ $mediaItem->getUrl() }}" alt="{{ $listing->name }}">
-                                </a>
-                            @endforeach
-                        </div>
-                    @endif
+                    <!-- Slider indicators -->
+                    <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+                        @for($i = 0; $i < count($listing->getMedia('default')); $i++)
+                            <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="{{$i}}"></button>
+                        @endfor
+                    </div>
+                    <!-- Slider controls -->
+                    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                            </svg>
+                            <span class="sr-only">Previous</span>
+                        </span>
+                                    </button>
+                                    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                            </svg>
+                            <span class="sr-only">Next</span>
+                        </span>
+                    </button>
                 </div>
-            </div>
 
             {{--make a container for the listing's details--}}
             <div class="container mx-auto w-full md:min-h-0 flex flex-col p-4 leading-normal">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$listing->name}}</h5>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{$listing->friendlyDescription()}}</p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{strip_tags($listing->description)}}</p>
                 <div class="flex flex-wrap">
                     {{--loop through attributes and show icons if they are true--}}
                     @foreach($listing->attributes as $key => $value)
