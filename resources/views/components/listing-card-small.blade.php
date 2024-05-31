8502b36bc75f55095e678a3fbf61af22a3becdf3
sjwatts119 <samuel.watts67@gmail.com>
2024-05-31 10:41:46 +0100

    Width Consistency Across Pages
    Consistently using 80% width for main page content

--- resources/views/components/listing-card-small.blade.php
+++ resources/views/components/listing-card-small.blade.php
@@ -18,10 +18,25 @@
     </div>
     <div class="w-full lg:min-h-0 flex flex-col p-4 leading-normal pr-10">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$listing->name}}</h5>
+        {{--add location with pin icon--}}
+        <div class="flex items-center pb-2">
+            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 22s-8-4.5-8-11a8 8 0 0 1 16 0c0 6.5-8 11-8 11z"/>
+                <circle cx="12" cy="10" r="3" fill="currentColor"/>
+            </svg>
+            <p class="ml-2 text-sm text-gray-700 dark:text-gray-400">{{$listing->location}}</p>
+        </div>
+        {{--add sleeps with bed svg icon--}}
+        <div class="flex items-center pb-2">
+            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" viewBox="0 0 14 14" role="img" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+                <path fill="currentColor" d="M 4.3,7 C 5.1270625,7 5.8,6.32706 5.8,5.5 5.8,4.67294 5.1270625,4 4.3,4 3.4729375,4 2.8,4.67294 2.8,5.5 2.8,6.32706 3.4729375,7 4.3,7 Z m 6.6,-2.4 -4.2,0 C 6.53425,4.6 6.4,4.73425 6.4,4.9 l 0,2.7 -4.2,0 0,-3.9 C 2.2,3.53425 2.06575,3.4 1.9,3.4 l -0.6,0 C 1.13425,3.4 1,3.53425 1,3.7 l 0,6.6 c 0,0.16575 0.13425,0.3 0.3,0.3 l 0.6,0 c 0.16575,0 0.3,-0.13425 0.3,-0.3 l 0,-0.9 9.6,0 0,0.9 c 0,0.16575 0.13425,0.3 0.3,0.3 l 0.6,0 c 0.16575,0 0.3,-0.13425 0.3,-0.3 L 13,6.7 C 13,5.54013 12.059875,4.6 10.9,4.6 Z"/>
+            </svg>
+            <p class="ml-2 text-sm text-gray-700 dark:text-gray-400">Sleeps {{$listing->sleeps}}</p>
+        </div>
         <div class="flex flex-wrap">
             @foreach($listing->attributes as $key => $value)
                 @if($value)
-                    <span class="inline-block bg-green-200 rounded-full px-3 py-1 my-1 text-sm font-semibold text-gray-700 mr-1">{{$key}}</span>
+                    <span class="inline-block bg-green-200 rounded-full px-3 py-1 my-1 text-xs font-semibold text-gray-700 mr-1">{{$key}}</span>
                 @endif
             @endforeach
         </div>
--- resources/views/properties/index.blade.php
+++ resources/views/properties/index.blade.php
@@ -8,16 +8,19 @@
         </h2>
     </x-slot>
 
-    {{--insert the featured-carousel component--}}
-    <x-featured-carousel :listings="$listings" />
+    {{--80% width container auto margin left and right --}}
+    <div class="container lg:w-3/4 w-11/12 mx-auto mt-5">
+        {{--insert the featured-carousel component--}}
+        <x-featured-carousel :listings="$listings" />
+    </div>
 
     {{--page divider--}}
-    <div class="container mx-auto px-4 py-5">
+    <div class="container mx-auto px-4 py-10">
         <hr class="border-t-2 border-gray-200 dark:border-gray-700">
     </div>
 
     {{--loop through the listings and show a small card each, with up to 5 per row--}}
-    <div class="container mx-auto px-4 py-5">
+    <div class="container lg:w-3/4 w-11/12 mx-auto">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             @foreach($listings as $listing)
                 <x-listing-card-small :listing="$listing" />
