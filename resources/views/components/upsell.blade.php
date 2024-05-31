46c0196e0d3cd176c923b455d9439d07d0da38f0
sjwatts119 <samuel.watts67@gmail.com>
2024-05-31 16:50:39 +0100

    Catching if no listings are present

--- resources/views/components/upsell.blade.php
+++ resources/views/components/upsell.blade.php
@@ -7,16 +7,22 @@
     $listings = $listings->shuffle()->take(4);
 @endphp
 
-{{--you may also like section--}}
-<div class="container mx-auto w-11/12 lg:w-3/4 pb-3">
-    <h2 class="text-3xl font-bold text-gray-900 dark:text-white">You may also like</h2>
-</div>
+@if($listings->count() > 0)
+    {{--you may also like section--}}
+    <div class="container mx-auto w-11/12 lg:w-3/4 pb-3">
+        <h2 class="text-3xl font-bold text-gray-900 dark:text-white">You may also like</h2>
+    </div>
 
-<div class="lg:w-3/4 w-11/12 mx-auto grid grid-cols-2 lg:grid-cols-4 gap-4 pb-5">
-    @foreach($listings as $listing)
-        <x-listing-card-upsell :listing="$listing" />
-    @endforeach
-</div>
+    <div class="lg:w-3/4 w-11/12 mx-auto grid grid-cols-2 lg:grid-cols-4 gap-4 pb-5">
+        @foreach($listings as $listing)
+            <x-listing-card-upsell :listing="$listing" />
+        @endforeach
+    </div>
+@else
+    <div class="container mx-auto w-11/12 lg:w-3/4 pb-10">
+        <h2 class="text-3xl font-bold text-gray-900 dark:text-white">No similar listings found</h2>
+    </div>
+@endif
 
 
 
--- resources/views/properties/index.blade.php
+++ resources/views/properties/index.blade.php
@@ -8,25 +8,31 @@
         </h2>
     </x-slot>
 
-    {{--80% width container auto margin left and right --}}
-    <div class="container lg:w-3/4 w-11/12 mx-auto mt-5">
-        {{--insert the featured-carousel component--}}
-        <x-featured-carousel :listings="$listings" />
-    </div>
-
-    {{--page divider--}}
-    <div class="container mx-auto px-4 py-10">
-        <hr class="border-t-2 border-gray-200 dark:border-gray-700">
-    </div>
-
-    {{--loop through the listings and show a small card each, with up to 5 per row--}}
-    <div class="container lg:w-3/4 w-11/12 mx-auto">
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            @foreach($listings as $listing)
-                <x-listing-card-small :listing="$listing" />
-            @endforeach
+    @if($listings->count() > 0)
+        {{--80% width container auto margin left and right --}}
+        <div class="container lg:w-3/4 w-11/12 mx-auto mt-5">
+            {{--insert the featured-carousel component--}}
+            <x-featured-carousel :listings="$listings" />
         </div>
-    </div>
+
+        {{--page divider--}}
+        <div class="container mx-auto px-4 py-10">
+            <hr class="border-t-2 border-gray-200 dark:border-gray-700">
+        </div>
+
+        {{--loop through the listings and show a small card each, with up to 5 per row--}}
+        <div class="container lg:w-3/4 w-11/12 mx-auto">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                @foreach($listings as $listing)
+                    <x-listing-card-small :listing="$listing" />
+                @endforeach
+            </div>
+        </div>
+    @else
+        <div class="container mx-auto w-11/12 lg:w-3/4 pb-10">
+            <h2 class="text-3xl font-bold text-gray-900 dark:text-white pt-10">No listings found.</h2>
+        </div>
+    @endif
 
 
 
