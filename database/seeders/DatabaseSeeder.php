2b668b5fad7b87b94852bedd55e852bca8ffa4ce
sjwatts119 <samuel.watts67@gmail.com>
2024-06-10 18:45:15 +0100

    Admin field in Users Table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,15 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true
+        ]);
+
+        User::factory()->create([
+            'name' => 'Test User 2',
+            'email' => 'user@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => false
         ]);
     }
 }
