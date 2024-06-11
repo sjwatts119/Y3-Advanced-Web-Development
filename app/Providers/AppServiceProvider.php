85d7f5480937e3fcf6d4cdc58f7bb6e2385a7dbe
sjwatts119 <samuel.watts67@gmail.com>
2024-06-11 18:19:30 +0100

    Theme for Login Page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     {
         View::composer(
             //pass the active theme to the navigation and footer layouts so we can use it in the views
-            ['layouts.navigation', 'layouts.footer'],
+            ['layouts.navigation', 'layouts.footer', 'layouts.guest'],
             //pass in the active theme from the themes model
             fn($view) => $view->with('theme', Theme::getTheme())
         );
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ $theme->company_name }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -18,7 +18,11 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    @if($theme->getFirstMediaUrl())
+                        <img src="{{ $theme->getFirstMediaUrl()}}" alt="{{ $theme->company_name }}" class="max-w-40 max-h-32" />
+                    @else
+                        <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    @endif
                 </a>
             </div>
 
