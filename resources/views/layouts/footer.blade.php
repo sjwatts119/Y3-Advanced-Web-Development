1ff7c88c3fcaed988a013c6f4898dec89f493179
sjwatts119 <samuel.watts67@gmail.com>
2024-06-11 17:51:45 +0100

    Footer updated with Custom Theming
    Custom theme elements now working with footer, including company name and logo

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,10 +3,14 @@
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 {{--insert the logo component--}}
-                <x-application-logo class="block h-8 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                @if($theme->getFirstMediaUrl())
+                    <img src="{{ $theme->getFirstMediaUrl()}}" alt="{{ $theme->company_name }}" class="max-w-12 max-h-12" />
+                @else
+                    <x-application-logo class="block h-8 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                @endif
                 {{--add the application name from the env file for now--}}
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                    {{ env('APP_NAME') }}
+                    {{ $theme->company_name }}
                 </span>
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
@@ -22,6 +26,6 @@
             </ul>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="{{route('home')}}" class="hover:underline">{{ env('APP_NAME') }}</a>. All Rights Reserved.</span>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="{{route('home')}}" class="hover:underline">{{ $theme->company_name }}</a>. All Rights Reserved.</span>
     </div>
 </footer>
