0a0ba5de99b6b42108313a5571dbd47b0021bbd3
sjwatts119 <samuel.watts67@gmail.com>
2024-06-11 18:01:31 +0100

    Themes for Nav
    Custom theme icon for Nav working

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,12 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('home') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        {{--insert the logo component--}}
+                        @if($theme->getFirstMediaUrl())
+                            <img src="{{ $theme->getFirstMediaUrl()}}" alt="{{ $theme->company_name }}" class="max-w-9 max-h-9" />
+                        @else
+                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        @endif
                     </a>
                 </div>
 
