20056b2113189e5621b02ba44e8f3538775f9480
sjwatts119 <samuel.watts67@gmail.com>
2024-06-13 21:49:33 +0100

    Create Booking Filament Resource
    Next I will be adding a status field to the bookings table, defaulting to "requested", we can then allow the admins to confirm bookings through the admin panel.

---
+++ app/Filament/Resources/BookingResource.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BookingResource\Pages;
+use App\Filament\Resources\BookingResource\RelationManagers;
+use App\Models\Booking;
+use App\Models\PropertyListing;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BookingResource extends Resource
+{
+    protected static ?string $model = Booking::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'User';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //we should have the following columns:
+                //Property Name, User Name, Start to End Date, Current Status
+                Tables\Columns\TextColumn::make('property_listing_id')
+                    ->label('Property Name')
+                    ->formatStateUsing(function (Booking $value) {return PropertyListing::class::find($value->property_listing_id)->name;}),
+                Tables\Columns\TextColumn::make('user_name')
+                    ->label('Name'),
+                Tables\Columns\TextColumn::make('start_date')
+                    ->label('Start to End Date')
+                    ->formatStateUsing(function (Booking $value) {return $value->start_date . ' - ' . $value->end_date;})
+                    ->badge()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBookings::route('/'),
+            'create' => Pages\CreateBooking::route('/create'),
+            'edit' => Pages\EditBooking::route('/{record}/edit'),
+        ];
+    }
+}
