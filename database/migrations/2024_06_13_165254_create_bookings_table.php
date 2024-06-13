fbe2e8ed26b9c9928c4fe54cf257b103cb8018d6
sjwatts119 <samuel.watts67@gmail.com>
2024-06-13 17:57:57 +0100

    Booking Model and Migration

---
+++ database/migrations/2024_06_13_165254_create_bookings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            //we need foreign key to PropertyListing, start date, end date, user's name, user's email, user's phone number (nullable), and a message
+            $table->foreignId('property_listing_id')->references('id')->on('property_listings');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('user_name');
+            $table->string('user_email');
+            $table->string('user_phone_number')->nullable();
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
