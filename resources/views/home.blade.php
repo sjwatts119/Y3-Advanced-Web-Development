538a286545b64e4cbfd4aef62d653939178a6130
sjwatts119 <samuel.watts67@gmail.com>
2024-06-14 23:57:22 +0100

    Add get in touch section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -103,6 +103,24 @@
         </div>
     </section>
 
+    <div class="bg-gradient-to-r from-blue-500 to-pink-500 pt-1"></div>
+
+    <section>
+        {{--get in touch jumbotron--}}
+        <div class="py-12 px-4 mx-auto max-w-screen-xl text-center">
+            <h2 class="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">Get In Touch</h2>
+            <p class="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400">We would love to hear from you. If you have any questions or would like to book a property, please get in touch.</p>
+            <a href="{{route('contact.index')}}" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-gradient-to-r from-blue-500 to-pink-500 hover:opacity-80 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Contact Us
+                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+        </div>
+    </section>
+
+
+
 </x-home-layout>
 
 
