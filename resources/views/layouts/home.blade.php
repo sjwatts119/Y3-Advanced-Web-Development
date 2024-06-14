50257607d0858fc69f76ebd3142370c6a1c728fd
sjwatts119 <samuel.watts67@gmail.com>
2024-06-14 19:39:36 +0100

    First Draft of Home View Background and nav
    issue #6

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -15,17 +15,11 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans antialiased">
-<div class="min-h-screen bg-gray-100 dark:bg-gray-900 pb-10">
-    @include('layouts.navigation')
+<div class="absolute inset-0 -z-10 h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
+<div class="absolute top-0 z-[-11] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
 
-    <!-- Page Heading -->
-    @if (isset($header))
-        <header class="bg-white dark:bg-gray-800 shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
-            </div>
-        </header>
-    @endif
+    <div class="min-h-screen pb-10">
+    @include('layouts.navigation-home')
 
     <!-- Page Content -->
     <main>
@@ -33,6 +27,5 @@
     </main>
 
 </div>
-@include('layouts.footer')
 </body>
 </html>
