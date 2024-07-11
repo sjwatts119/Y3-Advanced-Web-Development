9f7d072743f06f72c346224d65c9cd05fa379fe6
sjwatts119 <samuel.watts67@gmail.com>
2024-07-11 11:31:12 +0100

    Text Colour in Nav

--- resources/views/layouts/navigation-home.blade.php
+++ resources/views/layouts/navigation-home.blade.php
@@ -35,7 +35,7 @@
                 @if(Auth::user())
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
-                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-100 hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                                 <div>{{ Auth::user()->name }}</div>
 
                                 <div class="ms-1">
