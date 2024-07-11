11007b767cf43e5329ccce15e88f0b6038173d0e
sjwatts119 <samuel.watts67@gmail.com>
2024-07-11 14:40:12 +0100

    Local Area accordion element on Property Show view

--- resources/views/components/listing-main-accordion.blade.php
+++ resources/views/components/listing-main-accordion.blade.php
@@ -2,7 +2,7 @@
     <div x-data="{ expanded: false }" class="py-2">
         <h2>
             <button id="faqs-title-01" type="button" class="flex items-center justify-between w-full text-left font-semibold py-2" @click="expanded = !expanded" :aria-expanded="expanded" aria-controls="faqs-text-01">
-                <span>Property and Local Area</span>
+                <span>Property Info</span>
                 <svg class="fill-indigo-500 shrink-0 ml-8" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
                     <rect y="7" width="16" height="2" rx="1" class="transform origin-center transition duration-200 ease-out" :class="{'!rotate-180': expanded}" />
                     <rect y="7" width="16" height="2" rx="1" class="transform origin-center rotate-90 transition duration-200 ease-out" :class="{'!rotate-180': expanded}" />
@@ -17,7 +17,24 @@
             </div>
         </div>
     </div>
-    <!-- Accordion item -->
+    <div x-data="{ expanded: false }" class="py-2">
+        <h2>
+            <button id="faqs-title-01" type="button" class="flex items-center justify-between w-full text-left font-semibold py-2" @click="expanded = !expanded" :aria-expanded="expanded" aria-controls="faqs-text-01">
+                <span>The Local Area</span>
+                <svg class="fill-indigo-500 shrink-0 ml-8" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+                    <rect y="7" width="16" height="2" rx="1" class="transform origin-center transition duration-200 ease-out" :class="{'!rotate-180': expanded}" />
+                    <rect y="7" width="16" height="2" rx="1" class="transform origin-center rotate-90 transition duration-200 ease-out" :class="{'!rotate-180': expanded}" />
+                </svg>
+            </button>
+        </h2>
+        <div id="faqs-text-01" role="region" aria-labelledby="faqs-title-01" class="grid text-sm text-slate-600 overflow-hidden transition-all duration-300 ease-in-out" :class="expanded ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'">
+            <div class="overflow-hidden">
+                <p class="pb-3">
+                    {{strip_tags($listing->local_area)}}
+                </p>
+            </div>
+        </div>
+    </div>
     <div x-data="{ expanded: false }" class="py-2">
         <h2>
             <button id="faqs-title-01" type="button" class="flex items-center justify-between w-full text-left font-semibold py-2" @click="expanded = !expanded" :aria-expanded="expanded" aria-controls="faqs-text-01">
